706f5a4cc4cee7625dee87a104f0487dd2db95e4
Oscar Sánchez <osanpk@comunidad.unam.mx>
2018-04-24 18:08:28 -0500

    Basic D3 chart. Syntax correction.

--- js/tyw-scripts.js
+++ js/tyw-scripts.js
@@ -1,30 +1,38 @@
-var data = JSON.parse(php_vars);
+var data = JSON.parse(tyw_month_chart_data);
 
 var width = 600;
-height = d3.max(data, function(d){ return d.posts *100});
+height = 150;
 barWidth = 20;
 
-var xAxis = d3.axisBottom(data, function)
+var MaxPosts = d3.max(data, function (d) {return d.posts } );
 
 var yScale = d3.scaleLinear()
-    .domain([0, height])
-    .range([height, 0]);
+	.domain([0, MaxPosts])
+	.range([height, 0]);
 
-d3.select("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("width", barWidth )
-    .attr("height", function(d) {
-        return d.posts *100 ;
-    })
-    .attr("y", function (d) {
-        return yScale(d);
-    })
-    .attr("x", function (d, i) {
-        return ( barWidth + 10) *i;
-    })
-    .attr("fill", "black");
+
+var month_post_chart = d3.select("#tyw_month_chart")
+	.attr("width", width)
+	.attr("height", height );
+
+var month_post_chart_bar =  month_post_chart.selectAll("g")
+	.data(data)
+	.enter()
+	.append("g")
+	.attr("transform", function(d, i) {
+		return "translate(" + i * barWidth + ",0)"
+	});
+
+month_post_chart_bar.append("rect")
+	.attr("width", barWidth )
+	.attr("height", function(d) {
+		return height - yScale(d.posts)  ;
+	})
+	.attr("x", function (d, i) {
+		return ( barWidth + 5) *i;
+	})
+	.attr("y", function(d) { return yScale(d.posts) } )
+	.attr("fill", "green");
+
+
+
